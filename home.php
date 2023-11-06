4823c360af574df374aae4f73d435cfc052b2051
LUKAS1272 <zdarsky1272@gmail.com>
2023-11-06 12:54:01 +0100

    added account delete

--- home.php
+++ home.php
@@ -4,9 +4,6 @@
     if (isset($_SESSION["username"])) {
         echo "This is the home page <br>";
         echo "You are logged in as {$_SESSION["username"]}";
-    } else {
-        echo "You need to log in first!";
-        header("Location: index.php");
     }
 ?>
 
@@ -21,12 +18,31 @@
     <form method="post">
         <input type="submit" name="logout" value="Log out">
     </form>
+    <form method="post">
+        <label for="auth">Password authentication</label>
+        <input type="password" name="auth">
+
+        <input type="submit" name="delete" value="Delete account">
+    </form>
 </body>
 </html>
 
 <?php
+    $conn = mysqli_connect("localhost", "root", "", "jaknaweby.eu");
+
     if (!empty($_POST["logout"])) {
         session_destroy();
         header("Location: index.php");
+    } elseif (!empty($_POST["delete"])) {
+        $userInfo = mysqli_query($conn, "SELECT * FROM users WHERE username = '{$_SESSION["username"]}'");
+        $userFetch = mysqli_fetch_assoc($userInfo);
+
+        if (password_verify($_POST["auth"] . $userFetch["salt"], $userFetch["password"])) {
+            mysqli_query($conn, "DELETE FROM users WHERE username = '{$_SESSION["username"]}';");
+            session_destroy();
+            header("Location: index.php");
+        } else {
+            echo "Wrong password, try it again";
+        }
     }
 ?>
