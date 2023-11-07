2e8a9c4cdcad40f1e0aa40c989ed2b02e980298d
LUKAS1272 <zdarsky1272@gmail.com>
2023-11-07 13:49:10 +0100

    added styles

--- index.php
+++ index.php
@@ -1,40 +1,49 @@
 <?php
     session_start();
+
+    function writeMessage(string $someMessage) {
+        $message = $someMessage;
+        include("phpComponents/message.php");
+    }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@jaknaweby</title>
-    <link rel="stylesheet" href="tailwind/style.css">
-</head>
-<body>
-    <h1 class="underline">Test heading</h1>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>@jaknaweby</title>
+        <link rel="stylesheet" href="tailwind/style.css">
+    </head>
+    <body>
+        <form method="post" class="flex flex-col items-center">
+            <div class="flex flex-col">
+                <label class="text-lg" for="username">Username</label>
+                <input class="border-2 rounded text-lg pl-1" type="text" name="username" placeholder="Enter a username" required>
+            </div>
 
-    <form method="post">
-        <label for="username">Username</label>
-        <input type="text" name="username" placeholder="Enter a username" required><br>
-        
-        <label for="password">Password</label>
-        <input type="password" name="password" placeholder="Enter a password" required><br>
-        
-        <input type="submit" name="login" value="Log in">
-    </form>
+            <div class="flex flex-col mt-3">
+                <label class="text-lg" for="password">Password</label>
+                <input class="border-2 rounded text-lg pl-1" type="password" name="password" placeholder="Enter a password" required>
+            </div>
 
-    <br>
+            <input class="bg-zinc-300 px-7 py-1 rounded text-lg mt-5" type="submit" name="login" value="Log in">
+        </form>
 
-    <form method="post">
-        <label for="username">Username</label>
-        <input type="text" name="username" placeholder="Enter a username" required><br>
-        
-        <label for="password">Password</label>
-        <input type="password" name="password" placeholder="Enter a password" required><br>
-        
-        <input type="submit" name="register" value="Register">
-    </form>
-</body>
+        <form method="post" class="flex flex-col items-center mt-10">
+            <div class="flex flex-col">
+                <label class="text-lg" for="username">Username</label>
+                <input class="border-2 rounded text-lg pl-1" type="text" name="username" placeholder="Enter a username" required>
+            </div>
+
+            <div class="flex flex-col mt-3">
+                <label class="text-lg" for="password">Password</label>
+                <input class="border-2 rounded text-lg pl-1" type="password" name="password" placeholder="Enter a password" required>
+            </div>
+
+            <input class="bg-zinc-300 px-7 py-1 rounded text-lg mt-5" type="submit" name="register" value="Register">
+        </form>
+    </body>
 </html>
 
 <?php
@@ -48,49 +57,41 @@
 
             if (mysqli_num_rows($isValid) > 0) { // If the username was found
                 $isValidFetch = mysqli_fetch_assoc($isValid);
-                
 
                 if (password_verify($_POST["password"] . $isValidFetch["salt"], $isValidFetch["password"])) {
-                    echo "You are logged in as '{$_POST["username"]}'!";
-    
                     $_SESSION["username"] = $_POST["username"];
                     header("Location: home.php");
                 } else {
-
-                    echo $_POST["password"] . " " . $isValidFetch["salt"] . "<br>";
-                    echo password_hash($_POST["password"], PASSWORD_DEFAULT) . "<br>";
-                    echo password_verify(password_hash("LUKASEK127215533", PASSWORD_DEFAULT), $isValidFetch["password"]);
-                    echo "You entered wrong username or password <br>";
+                    writeMessage("Invalid username or password");
                 }
             } else {
-                echo "You entered wrong username or password <br>";
+                writeMessage("Invalid username or password");
             }
         } elseif (isset($_POST["register"])) {
             $isTaken = mysqli_query($conn, "SELECT * FROM users WHERE username = '{$_POST["username"]}'");
 
             if (mysqli_num_rows($isTaken) == 0) { // If the username was not found
                 // RegEx patterm
-                // - at least one upper letter
-                // - at least one lower letter
+                // - at least one uppercase letter
+                // - at least one lowercase letter
                 // - at least one number
                 // - no whitespace
                 // - at least 12 characters
                 $pattern = "/(?=.*?[A-Z])(?=.*?[a-z])(?=.*?\d)(?!.*?[\s]).{12,}/";
                 if (preg_match($pattern, $_POST["password"])) {
                     $salt = strval(rand(10000, 100000));
-                    echo $salt . "<br>";
+                    // echo $salt . "<br>";
 
                     $hashedPassword = password_hash($_POST["password"] . $salt, PASSWORD_DEFAULT);
-                    echo $hashedPassword . "<br>";
-                    
+                    // echo $hashedPassword . "<br>";
+
                     mysqli_query($conn, "INSERT INTO users (`username`, `password`, `salt`) VALUES ('{$_POST["username"]}', '{$hashedPassword}', '{$salt}');");
+                    writeMessage("Succesfully registered");
                 } else {
-                    echo "Password not secure enough, it must contain <br>- at least 1 upper letter <br>- at least 1 lower letter <br>- at least 1 number <br>- at least 12 characters <br>- no whitespace <br>";
+                    writeMessage("Password not secure enough, it must contain <br>- at least 1 uppercase letter <br>- at least 1 lowercase letter <br>- at least 1 number <br>- at least 12 characters <br>- no whitespace <br>");
                 }
-                
-                
             } else {
-                echo "User with username {$_POST["username"]} already exists";
+                writeMessage("User with username {$_POST["username"]} already exists");
             }
         }
     }
---
+++ phpComponents/message.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="../tailwind/style.css">
+    </head>
+    <body>
+        <div class="flex text-red-800 w-fit mx-auto mt-10"><?php echo $message; ?></div>
+    </body>
+</html>
