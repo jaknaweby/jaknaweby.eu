727f24532c5601ce603f0496090e953111e135d0
LUKAS1272 <zdarsky1272@gmail.com>
2023-11-14 21:18:29 +0100

    added automatic footer

---
+++ phpComponents/footer.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div style="margin-top: auto;"></div>
+    <footer class="text-white bg-stone-950 py-16 px-10 mt-36 sm:px-14 md:px-20 lg:px-24 xl:px-28 2xl:px-32">
+        <ul class="flex justify-center mx-auto mb-7">
+            <li class="w-16 mr-3"><a href="https://www.instagram.com/jaknaweby/"><?php echo "<img src='{$path}/instagram_logo.png' alt='instagram logo'>"; ?></a></li>
+            <li class="w-16 mr-3"><a href="https://dsc.gg/jaknaweby"><?php echo "<img src='{$path}/discord_logo.png' alt='discord logo'>"; ?></a></li>
+            <li class="w-16 mr-3"><a href="https://www.youtube.com/jaknaweby"><?php echo "<img src='{$path}/youtube_logo.png' alt='youtube logo'>"; ?></a></li>
+            <li class="w-16"><a href="https://tiktok.com/@jaknaweby"><?php echo "<img src='{$path}/tiktok_logo.png' alt='tiktok logo'>"; ?></a></li>
+        </ul>
+        <p class="font-extralight text-center text-xl">© 2023 @jaknaweby</p>
+    </footer>
+</body>
+
+</html>
