38623df4f337d84b1a482a6c3a011b3032fcd0ab
LUKAS1272 <zdarsky1272@gmail.com>
2023-11-15 18:48:53 +0100

    edited navbar in header menu

--- phpComponents/header.php
+++ phpComponents/header.php
@@ -1,8 +1,3 @@
-<?php
-    $lang = "html";
-    $title = "Co je to HTML";
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,43 +9,45 @@
 </head>
 <body>
     <header>
-        <nav class="w-screen bg-gray-950 flex items-center justify-center h-16">
+        <nav class="w-screen bg-stone-950 flex items-center justify-center h-12 relative">
+            <a class="h-full px-6 flex items-center bg-zinc-900/20 hover:bg-zinc-900 absolute top-0 left-0"><img src="<?php echo "{$path}"; ?>/img/more.png" alt="hamburger icon" class="h-1/2"></a>
+
             <ul class="flex text-white items-center text-xl h-full">
-                <li class="h-full"><a href=".." class="py-auto h-full px-6 flex items-center bg-zinc-900/20 hover:bg-zinc-900"><img src="../img/home.png" alt="" class="h-1/2"></a></li>
+                <li class="h-full"><a href="../" class="h-full px-6 flex items-center bg-zinc-900/20 hover:bg-zinc-900"><img src="<?php echo "{$path}"; ?>/img/home.png" alt="home icon" class="h-1/2"></a></li>
                 
                 <li class="h-full">
-                    <a href="" class="py-auto h-full px-6 flex items-center bg-red-600<?php if ($lang != "html") { echo "/20"; } ?> hover:bg-red-600">
+                    <a <?php if ($lang != "html") { echo "href='../html'"; } else { echo "href='./'"; } ?> class="font-light py-auto h-full px-6 flex items-center bg-red-600<?php if ($lang != "html") { echo "/20"; } ?> hover:bg-red-600">
                         HTML
                     </a>
                 </li>
                 
                 <li class="h-full">
-                    <a href="#" class="py-auto h-full px-6 flex items-center bg-sky-600<?php if ($lang != "css") { echo "/20"; } ?> hover:bg-sky-600">
+                    <a <?php if ($lang != "css") { echo "href='../css'"; } else { echo "href='../'"; } ?> class="font-light py-auto h-full px-6 flex items-center bg-sky-600<?php if ($lang != "css") { echo "/20"; } ?> hover:bg-sky-600">
                         CSS
                     </a>
                 </li>
                 
                 <li class="h-full">
-                    <a href="#" class="py-auto h-full px-6 flex items-center bg-amber-500<?php if ($lang != "js") { echo "/20"; } ?> hover:bg-amber-500">
+                    <a <?php if ($lang != "js") { echo "href='../javascript'"; } else { echo "href='../'"; } ?> class="font-light py-auto h-full px-6 flex items-center bg-amber-500<?php if ($lang != "js") { echo "/20"; } ?> hover:bg-amber-500">
                         JavaScript
                     </a>
                 </li>
                 
                 <li class="h-full">
-                    <a href="#" class="py-auto h-full px-6 flex items-center bg-indigo-600<?php if ($lang != "php") { echo "/20"; } ?> hover:bg-indigo-600">
+                    <a <?php if ($lang != "php") { echo "href='../php'"; } else { echo "href='../'"; } ?> class="font-light py-auto h-full px-6 flex items-center bg-indigo-600<?php if ($lang != "php") { echo "/20"; } ?> hover:bg-indigo-600">
                         PHP
                     </a>
                 </li>
                 
                 <li class="h-full">
-                    <a href="#" class="py-auto h-full px-6 flex items-center bg-orange-600<?php if ($lang != "sql") { echo "/20"; } ?> hover:bg-orange-600">
+                    <a <?php if ($lang != "sql") { echo "href='../sql'"; } else { echo "href='../'"; } ?> class="font-light py-auto h-full px-6 flex items-center bg-orange-600<?php if ($lang != "sql") { echo "/20"; } ?> hover:bg-orange-600">
                         SQL
                     </a>
                 </li>
             </ul>
         </nav>
         <div class="flex justify-center bg-<?php if ($lang === "html") { echo "red-600"; } else if ($lang === "css") { echo "sky-600"; } else if ($lang === "js") { echo "amber-500"; } else if ($lang === "php") { echo "indigo-600"; } else if ($lang === "sql") { echo "orange-600"; } ?>">
-            <h1 class="text-5xl font-bold text-white py-24"><?php echo "{$title}"; ?></h1>
+            <h1 class="text-5xl font-bold text-white py-20"><?php echo "{$title}"; ?></h1>
         </div>
     </header>
 </body>
---
+++ tutorials/html/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="../../css/style.css">
+    <link rel="stylesheet" href="../../css/global.css">
+    <link rel="icon" type="image/x-icon" href="../img/jaknaweby_logo.png">
+</head>
+
+<body class="flex flex-col min-h-screen">
+    <?php
+    $lang = "html";
+    $title = "Co je to HTML";
+    $path = "../../";
+    include("../../phpComponents/header.php");
+    include("../../phpComponents/footer.php");
+    ?>
+</body>
+
+</html>
