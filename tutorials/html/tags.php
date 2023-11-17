d05f9b8c4c75ef7ee5957c0adc86306006454f70
LUKAS1272 <zdarsky1272@gmail.com>
2023-11-17 21:57:22 +0100

    added article about tags

---
+++ tutorials/html/tags.php
@@ -0,0 +1,26 @@
+<?php
+            $lang = "html";
+            $title = "Tagy";
+            $path = "../../";
+        ?>
+        
+        <!DOCTYPE html>
+        <html lang="en">
+        
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title><?php echo "{$title}"; ?></title>
+        
+            <link rel="stylesheet" href="../../css/style.css">
+            <link rel="stylesheet" href="../../css/global.css">
+            <link rel="icon" type="image/x-icon" href="../../img/jaknaweby_logo.png">
+        </head>
+        
+        <body class="flex flex-col min-h-screen">
+            <?php include("../../phpComponents/header.php"); ?>
+        
+            <?php include("../../phpComponents/footer.php"); ?>
+        </body>
+        
+        </html>
