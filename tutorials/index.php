a7aaa54971fa19b89d3e808ed18396390511f500
LUKAS1272 <zdarsky1272@gmail.com>
2023-11-17 21:53:28 +0100

    added basic nav to tutorials index page

--- tutorials/index.php
+++ tutorials/index.php
@@ -5,9 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="../css/global.css">
-    <title>jaknaweby tutorialy</title>
+    <title>@jaknaweby</title>
 </head>
 <body>
-    
+    <ul>
+        <li><a href="html">HTML</a></li>
+        <li><a href="css">CSS</a></li>
+        <li><a href="javascript">JavaScript</a></li>
+        <li><a href="php">PHP</a></li>
+        <li><a href="sql">SQL</a></li>
+    </ul>
 </body>
 </html>
