0d004480b3f2ad57ea52bd7ba297b35b93d14d25
LUKAS1272 <zdarsky1272@gmail.com>
2023-11-18 17:40:58 +0100

    added index files for all languages

---
+++ tutorials/sql/index.php
@@ -0,0 +1,27 @@
+<?php
+    $lang = "sql";
+    $title = "Co je to SQL";
+    $path = "../../";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo "{$title}"; ?></title>
+
+    <link rel="stylesheet" href="../../css/style.css">
+    <link rel="stylesheet" href="../../css/global.css">
+    <link rel="icon" type="image/x-icon" href="../../img/jaknaweby_logo.png">
+</head>
+
+<body>
+    <?php
+    include("../../phpComponents/header.php");
+    include("../../phpComponents/footer.php");
+    ?>
+</body>
+
+</html>
