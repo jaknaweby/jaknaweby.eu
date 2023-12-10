145016524902558eddcafe26b122bc951b036750
LUKAS1272 <zdarsky1272@gmail.com>
2023-12-10 19:54:21 +0100

    created tutorial page structure

---
+++ resources/views/layouts/template.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/global.css') }}">
+    <link rel="icon" href="{{ asset('img/jaknaweby_logo.png') }}">
+    <title>@yield('title')</title>
+</head>
+<body>
+    @yield('content')
+
+    <div style="margin-top: auto;"></div>
+    <footer class="text-white bg-stone-950 py-16 px-10 mt-36 sm:px-14 md:px-20 lg:px-24 xl:px-28 2xl:px-32">
+        <ul class="flex justify-center mx-auto mb-7">
+            <li class="w-16 mr-3"><a href="https://www.instagram.com/jaknaweby/"><img src="{{ asset('img/instagram_logo.png') }}" alt="instagram logo"></a></li>
+            <li class="w-16 mr-3"><a href="https://dsc.gg/jaknaweby"><img src="{{ asset('img/discord_logo.png') }}" alt="discord logo"></a></li>
+            <li class="w-16 mr-3"><a href="https://www.youtube.com/jaknaweby"><img src="{{ asset('img/youtube_logo.png') }}" alt="youtube logo"></a></li>
+            <li class="w-16"><a href="https://tiktok.com/@jaknaweby"><img src="{{ asset('img/tiktok_logo.png') }}" alt="tiktok logo"></a></li>
+        </ul>
+        <p class="font-extralight text-center text-xl">© 2023 @jaknaweby</p>
+    </footer>
+
+    @yield('script')
+</body>
+</html>
--- resources/views/tutorialPage.blade.php
+++ resources/views/tutorialPage.blade.php
@@ -1,3 +1,126 @@
-<div>
-    {{$lang}}
-</div>
+@extends('layouts.template')
+
+@section('title')
+    {{ ucfirst($article) }}
+@endsection
+
+@section('content')
+    <header class="sticky top-0">
+        <nav class="w-full bg-stone-950 flex items-center justify-center h-12 relative">
+            <a class="h-full px-6 flex items-center bg-zinc-900/20 hover:bg-zinc-900 absolute top-0 left-0 cursor-pointer" id="hamburger"><img src="{{ asset('img/hamburger.png') }}" alt="hamburger icon" class="h-1/2" id="hamburger-logo"></a>
+    
+            <ul class="flex text-white items-center text-xl h-full">
+                <li class="h-full"><a href="../" class="h-full px-6 flex items-center bg-zinc-900/20 hover:bg-zinc-900"><img src="{{ asset('img/home.png') }}" alt="home icon" class="h-1/2"></a></li>
+    
+                <li class="h-full">
+                    <a @if($lang != "html") href='../html' @else href='./' @endif class="font-light py-auto h-full px-6 flex items-center @if($lang != "html") bg-red-600/20 hover:bg-red-600 @else bg-red-600 @endif">
+                        HTML
+                    </a>
+                </li>
+    
+                <li class="h-full">
+                    <a @if($lang != "css") href='../html' @else href='./' @endif class="font-light py-auto h-full px-6 flex items-center @if($lang != "css") bg-sky-600/20 hover:bg-sky-600 @else bg-sky-600 @endif">
+                        CSS
+                    </a>
+                </li>
+    
+                <li class="h-full">
+                    <a @if($lang != "javascript") href='../html' @else href='./' @endif class="font-light py-auto h-full px-6 flex items-center @if($lang != "javascript") bg-yellow-600/20 hover:bg-yellow-600 @else bg-yellow-600 @endif">
+                        JavaScript
+                    </a>
+                </li>
+    
+                <li class="h-full">
+                    <a @if($lang != "php") href='../html' @else href='./' @endif class="font-light py-auto h-full px-6 flex items-center @if($lang != "php") bg-indigo-600/20 hover:bg-indigo-600 @else bg-indigo-600 @endif">
+                        PHP
+                    </a>
+                </li>
+    
+                <li class="h-full">
+                    <a @if($lang != "sql") href='../html' @else href='./' @endif class="font-light py-auto h-full px-6 flex items-center @if($lang != "sql") bg-orange-600/20 hover:bg-orange-600 @else bg-orange-600 @endif">
+                        SQL
+                    </a>
+                </li>
+            </ul>
+    
+            <div class="absolute top-0 right-0 flex h-full">
+                <?php
+                    // session_start();
+    
+                    // try {
+                    //     $connection = mysqli_connect("localhost", "root", "", "jaknaweby.eu");
+                    //     $queryResult = mysqli_query($connection, "SELECT isAdmin FROM users WHERE username = '{$_SESSION["username"]}'");
+            
+                    //     if (mysqli_num_rows($queryResult) > 0) {
+                    //         $fetchedResult = mysqli_fetch_row($queryResult);
+                            
+                    //         if ($fetchedResult[0] == 1) {
+                    //             echo "<a href=\"../article.php\" class=\"h-full px-6 flex items-center bg-zinc-900/20 hover:bg-zinc-900\"><img src=\"{$path}/img/article.png\" alt=\"article icon\" class=\"h-1/2\"></a>";
+                    //         }
+                    //     }
+                    // } catch (Exception $e) {}
+                ?>
+                <a href="../login.php" class="h-full px-6 flex items-center bg-zinc-900/20 hover:bg-zinc-900"><img src="{{ asset('img/user.png') }}" alt="user icon" class="h-1/2"></a>
+            </div>
+        </nav>
+    
+        <nav class="w-1/6 bg-stone-200 fixed top-12 bottom-0 shadow-2xl hidden items-center flex-col" id="articles">
+            <h3 class="text-2xl font-semibold m-7">
+                @if($lang === "html")
+                    HTML
+                @elseif($lang === "css")
+                    CSS
+                @elseif($lang === "javascript")
+                    JavaScript
+                @elseif($lang === "php")
+                    PHP
+                @elseif($lang === "sql")
+                    SQL
+                @endif
+            </h3>
+            <ul class="w-full font-light">
+                <?php
+                // try {
+                //     $connection = mysqli_connect("localhost", "root", "", "jaknaweby.eu");
+                //     $articles = mysqli_query($connection, "SELECT title, url FROM articles_{$lang}");
+    
+                //     foreach (mysqli_fetch_all($articles) as $row) {
+                //         $itemTitle = $row[0];
+                //         $itemUrl = $row[1];
+                //         include("../../phpComponents/navItem.php");
+                //     }
+                // } catch (Exception $e) {
+                //     echo $e;
+                // }
+                ?>
+            </ul>
+        </nav>
+    </header>
+    
+    <div class="flex justify-center @if($lang === "html") bg-red-600 @elseif ($lang === "css") bg-sky-600 @elseif ($lang === "javascript") bg-yellow-600 @elseif ($lang === "php") bg-indigo-600 @elseif ($lang === "sql") bg-orange-600 @endif">
+        <h1 class="text-5xl font-bold text-white py-20">{{ $article }}</h1>
+    </div>
+@endsection
+
+@section('script')
+    <script>
+        let hamburger = document.querySelector("#hamburger");
+        let hamburgerLogo = document.querySelector("#hamburger-logo");
+        let navbar = document.querySelector("#articles");
+        let turnedOn = false;
+
+        hamburger.addEventListener("click", function() {
+            if (!turnedOn) {
+                navbar.style.display = "flex";
+                hamburgerLogo.src = "{{ asset('img/cross.jpg') }}";
+                hamburgerLogo.alt = "cross icon";
+            } else {
+                navbar.style.display = "";
+                hamburgerLogo.src = "{{ asset('img/hamburger.png') }}";
+                hamburgerLogo.alt = "hamburger icon";
+            }
+
+            turnedOn = !turnedOn
+        });
+    </script>
+@endsection
