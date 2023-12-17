3e2f032b63151ab2bca70e9dd584066930f434ab
LUKAS1272 <zdarsky1272@gmail.com>
2023-12-17 20:34:03 +0100

    moved function showArticle to article controller

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -6,16 +6,5 @@ use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
-    public function showArticle($lang, $article = "") {
-        if ($lang == "html" || $lang == "css" || $lang == "javascript" || $lang == "php" || $lang == "sql") {
 
-            if ($article != '') {
-                return view('tutorialPage',['lang' => $lang, 'article' => $article, 'isIndex' => false]);
-            } else {
-                return view('tutorialPage',['lang' => $lang, 'article' => "Co je to " . strtoupper($lang), 'isIndex' => true]);
-            }
-        } else {
-            return abort(404);
-        }
-    }
 }
