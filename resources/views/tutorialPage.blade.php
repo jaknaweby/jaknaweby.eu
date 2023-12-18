0762ea6230f126b6b25595cba1eddde23b4bbf24
LUKAS1272 <zdarsky1272@gmail.com>
2023-12-18 15:41:34 +0100

    fixed faulty links

--- resources/views/tutorialPage.blade.php
+++ resources/views/tutorialPage.blade.php
@@ -14,31 +14,31 @@
     
                 <li class="h-full">
                     {{-- Fix linking --}}
-                    <a href="@if($isIndex == false && $lang == "html") . @elseif($lang != "html") html @endif" class="font-light py-auto h-full px-6 flex items-center @if($lang != "html") bg-red-600/20 hover:bg-red-600 @else bg-red-600 @endif">
+                    <a href="@if($article->filename != NULL && $article->language == "html") . @elseif($article->filename != NULL && $article->language != "html") ../html @elseif($article->filename == NULL && $article->language != "html") ./html @endif" class="font-light py-auto h-full px-6 flex items-center @if($article->language != "html") bg-red-600/20 hover:bg-red-600 @else bg-red-600 @endif">
                         HTML
                     </a>
                 </li>
     
                 <li class="h-full">
-                    <a href="@if($isIndex == false && $lang == "css") . @elseif($lang != "css") css @endif" class="font-light py-auto h-full px-6 flex items-center @if($lang != "css") bg-sky-600/20 hover:bg-sky-600 @else bg-sky-600 @endif">
+                    <a href="@if($article->filename != NULL && $article->language == "css") . @elseif($article->filename != NULL && $article->language != "css") ../css @elseif($article->filename == NULL && $article->language != "css") ./css @endif" class="font-light py-auto h-full px-6 flex items-center @if($lang != "css") bg-sky-600/20 hover:bg-sky-600 @else bg-sky-600 @endif">
                         CSS
                     </a>
                 </li>
     
                 <li class="h-full">
-                    <a href="@if($isIndex == false && $lang == "javascript") . @elseif($lang != "javascript") javascript @endif" class="font-light py-auto h-full px-6 flex items-center @if($lang != "javascript") bg-yellow-600/20 hover:bg-yellow-600 @else bg-yellow-600 @endif">
+                    <a href="@if($article->filename != NULL && $article->language == "javascript") . @elseif($article->filename != NULL && $article->language != "javascript") ../javascript @elseif($article->filename == NULL && $article->language != "javascript") ./javascript @endif" class="font-light py-auto h-full px-6 flex items-center @if($lang != "javascript") bg-yellow-600/20 hover:bg-yellow-600 @else bg-yellow-600 @endif">
                         JavaScript
                     </a>
                 </li>
     
                 <li class="h-full">
-                    <a href="@if($isIndex == false && $lang == "php") . @elseif($lang != "php") php @endif" class="font-light py-auto h-full px-6 flex items-center @if($lang != "php") bg-indigo-600/20 hover:bg-indigo-600 @else bg-indigo-600 @endif">
+                    <a href="@if($article->filename != NULL && $article->language == "php") . @elseif($article->filename != NULL && $article->language != "php") ../php @elseif($article->filename == NULL && $article->language != "php") ./php @endif" class="font-light py-auto h-full px-6 flex items-center @if($lang != "php") bg-indigo-600/20 hover:bg-indigo-600 @else bg-indigo-600 @endif">
                         PHP
                     </a>
                 </li>
     
                 <li class="h-full">
-                    <a href="@if($isIndex == false && $lang == "sql") . @elseif($lang != "sql") sql @endif" class="font-light py-auto h-full px-6 flex items-center @if($lang != "sql") bg-orange-600/20 hover:bg-orange-600 @else bg-orange-600 @endif">
+                    <a href="@if($article->filename != NULL && $article->language == "sql") . @elseif($article->filename != NULL && $article->language != "sql") ../sql @elseif($article->filename == NULL && $article->language != "sql") ./sql @endif" class="font-light py-auto h-full px-6 flex items-center @if($lang != "sql") bg-orange-600/20 hover:bg-orange-600 @else bg-orange-600 @endif">
                         SQL
                     </a>
                 </li>
@@ -66,8 +66,11 @@
             </h3>
             <ul class="w-full font-light">
                 @foreach($articles as $artic)
-                    {{-- Implement background color for all languages --}}
-                    <li class="w-full py-0.5 pl-3 @if($artic->title == $article->title) bg-red-500 @endif">{{ $artic->title }}</li>
+                    <li class="w-full @if($artic->title == $article->title) text-white @if($artic->language == "html") bg-red-600 @elseif($artic->language == "css") bg-sky-600 @elseif($artic->language == "javascript") bg-yellow-600 @elseif($artic->language == "php") bg-indigo-600 @elseif($artic->language == "sql") bg-orange-600 @endif @else hover:bg-gray-300 @endif">
+                        <a href="@if($article->filename == NULL) {{ $artic->language }}/{{ $artic->filename }} @elseif($artic->filename == NULL) . @else {{ $artic->filename }} @endif" class="w-full h-full inline-block py-0.5 pl-3">
+                            {{ $artic->title }}
+                        </a>
+                    </li>
                 @endforeach
             </ul>
         </nav>
