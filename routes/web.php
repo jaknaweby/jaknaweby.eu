c0a4f1ce7499a32c2ea28e7474f54a6205674238
LUKAS1272 <zdarsky1272@gmail.com>
2023-12-20 08:10:28 +0100

    added named routes

--- routes/web.php
+++ routes/web.php
@@ -4,29 +4,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PagesController;
 use App\Http\Controllers\ArticlesController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'index')->name('index');
 
-// Route::get('tutorials/{lang}/{article}', [PagesController::class, 'showArticle']);
-// Route::get('tutorials/{lang}', [PagesController::class, 'showArticle']);
-
-// Route::get('tutorials', function () {
-//     return "Tutorials main page";
-// });
-
-Route::get('tutorials', [ArticlesController::class, 'index']);
+Route::get('tutorials', [ArticlesController::class, 'index'])->name('tutorials');
+Route::view('tutorials/login', 'auth.login')->name('login');
 
 Route::get('tutorials/{lang}', [ArticlesController::class, 'show']);
 Route::get('tutorials/{lang}/{filename}', [ArticlesController::class, 'show']);
