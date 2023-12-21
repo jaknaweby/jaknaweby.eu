0353e57e4ac17069f199e03765daad6e3d29c080
LUKAS1272 <zdarsky1272@gmail.com>
2023-12-21 11:07:13 +0100

    created own basic login / register page

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ Route::view('/', 'index')->name('index');
 
 Route::get('tutorials', [ArticlesController::class, 'index'])->name('tutorials');
 Route::view('tutorials/login', 'auth.login')->name('login');
+Route::view('tutorials/register', 'auth.register')->name('register');
 
 Route::get('tutorials/{lang}', [ArticlesController::class, 'show']);
 Route::get('tutorials/{lang}/{filename}', [ArticlesController::class, 'show']);
