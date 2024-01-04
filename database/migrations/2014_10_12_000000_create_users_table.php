b856a419054a34b95db003284a0d4f754411f8a4
LUKAS1272 <zdarsky1272@gmail.com>
2024-01-04 14:27:30 +0100

    added breeze login template

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -6,17 +6,25 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+    /**
+     * Run the migrations.
+     */
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('username');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('salt');
-            $table->boolean('isAdmin')->default(0);
+            $table->rememberToken();
+            $table->timestamps();
         });
     }
 
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
         Schema::dropIfExists('users');
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,28 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PagesController;
 use App\Http\Controllers\ArticlesController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
 
+
+
+/* Revise - old routes (before installing breeze) */
 Route::view('/', 'index')->name('index');
 
 Route::get('tutorials', [ArticlesController::class, 'index'])->name('tutorials');
@@ -13,3 +31,7 @@ Route::view('tutorials/register', 'auth.register')->name('register');
 
 Route::get('tutorials/{lang}', [ArticlesController::class, 'show']);
 Route::get('tutorials/{lang}/{filename}', [ArticlesController::class, 'show']);
+/* --- */
+
+
+require __DIR__.'/auth.php';
