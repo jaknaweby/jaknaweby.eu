123491ddba1dae70480d2f0b3ec6de85a76333af
LUKAS1272 <zdarsky1272@gmail.com>
2024-02-02 22:59:42 +0100

    made minor style changes

--- resources/views/components/input-label.blade.php
+++ resources/views/components/input-label.blade.php
@@ -1,6 +1,6 @@
 @props(['value'])
 
 
-<label {{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-700']) }}>
-    {{ $value ?? "Fail" }}
+<label {{ $attributes->merge(['class' => 'block font-medium text-sm']) }}>
+    {{ $value }}
 </label>
