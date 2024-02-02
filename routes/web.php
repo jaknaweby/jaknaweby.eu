6923ccff5e7fbb292158e35c12b7d8fcd1e5b3d4
LUKAS1272 <zdarsky1272@gmail.com>
2024-02-02 22:47:42 +0100

    edited web routes for breeze

--- routes/web.php
+++ routes/web.php
@@ -5,33 +5,24 @@ use App\Http\Controllers\ArticlesController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
+Route::get('tutorials/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('tutorials/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('tutorials/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('tutorials/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 
+// Loads auth routes sooner than the web ones
+require __DIR__.'/auth.php';
 
 
-/* Revise - old routes (before installing breeze) */
 Route::view('/', 'index')->name('index');
 
 Route::get('tutorials', [ArticlesController::class, 'index'])->name('tutorials');
-Route::view('tutorials/login', 'auth.login')->name('login');
-Route::view('tutorials/register', 'auth.register')->name('register');
 
 Route::get('tutorials/{lang}', [ArticlesController::class, 'show']);
 Route::get('tutorials/{lang}/{filename}', [ArticlesController::class, 'show']);
-/* --- */
-
-
-require __DIR__.'/auth.php';
